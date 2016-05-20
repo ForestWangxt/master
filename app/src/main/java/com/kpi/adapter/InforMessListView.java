49f50a35fb11f6a7e02fcaadd78cc791fd2b0c4a
forestwang <136684667@qq.com>
2016-05-20 11:23:18 +0800

    xx

---
+++ app/src/main/java/com/kpi/adapter/InforMessListView.java
@@ -0,0 +1,68 @@
+package com.kpi.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.kpi.bean.InforMess;
+import com.storm.kpi.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/5/19.
+ */
+public class InforMessListView extends BaseAdapter {
+    private Context context;
+    private ArrayList<InforMess> mArrayList = new ArrayList<>();
+    public InforMessListView(Context mcontext, ArrayList<InforMess> list){
+        this.context = mcontext;
+        this.mArrayList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return mArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        System.out.println("=======aaaaa==========");
+        System.out.println(mArrayList.size());
+        ViewHolder viewHolder = null;
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.informess_list_item, null);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.infor_message_title);
+            viewHolder.content = (TextView) convertView.findViewById(R.id.infor_message_abstract);
+            viewHolder.more = (TextView) convertView.findViewById(R.id.infor_message_more);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.title.setText(mArrayList.get(position).getTitle());
+        viewHolder.content.setText(mArrayList.get(position).getAbst());
+        System.out.println("======"+position);
+        System.out.println(mArrayList.get(position).getTitle());
+        System.out.println(mArrayList.get(position).getAbst());
+        return convertView;
+    }
+    static class ViewHolder {
+        TextView title;
+        TextView content;
+        TextView more;
+    }
+}
